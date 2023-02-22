da8bdcb952fe6ef142845921c47b769bd62023fb
Theresa <theresaolusoga@Bolajis-MacBook-Pro.local>
2023-02-22 17:54:31 -0500

    Accept input from user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,34 @@
 // Select Button with Id generate
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.getElementById("generate");
 
 // Event Listener When generate button is clicked
 generateBtn.addEventListener("click", writePassword);
 
 // Function to prompt for password criteria and generate a password
 function writePassword() {
+  //  get returned password from generatePassword function 
   var password = generatePassword();
+
+
   var passwordText = document.querySelector("#password");
+
 
   passwordText.value = password;
 
+}
+
+function generatePassword(){
+
+  //prompt user to enter desired password length and check to validate
+
+  var passwordLenght = prompt("Enter password Lenght Between 8 and 128 characters");
+
+  while(isNaN(passwordLenght) ||  passwordLenght < 8 || passwordLenght>128){
+    // prompts an error when an invalid input is entered
+    var passwordLenght = prompt("Invalid input Entered! Numbers between 8 and 128 is expected ")
+  }
+
+   
 }
 
 
