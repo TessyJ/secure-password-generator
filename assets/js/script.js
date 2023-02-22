66456777969d544e1f8f14411bddfe2e78437739
Theresa <theresaolusoga@Bolajis-MacBook-Pro.local>
2023-02-22 18:06:46 -0500

    Ask for password criteria and validate

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,22 @@ function generatePassword(){
     var passwordLenght = prompt("Invalid input Entered! Numbers between 8 and 128 is expected ")
   }
 
-   
+    // Asks for password criteria from users
+    var lowercase = confirm("Include lowercase characters?");
+    var uppercase = confirm("Include uppercase characters?");
+    var numeric = confirm("Include numeric characters?");
+    var special = confirm("Include special characters?");
+
+    //checks if at least one password criteria was selected
+    while(!lowercase && !uppercase && !numeric && !special){
+      alert("You have to select at least one password criteria")
+      var lowercase = confirm("Include lowercase characters?");
+      var uppercase = confirm("Include uppercase characters?");
+      var numeric = confirm("Include numeric characters?");
+      var special = confirm("Include special characters?");
+    }
+
+    console.log("Next Code Goes here")
 }
 
 
